5a8263ff80f182417dd2759ccef735fa412212c4
vagrant <vivien.fan.1224@gmail.com>
2017-06-08 19:19:19 +0000

    replace hardcoded path with routes helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to root_path
     else
-      redirect_to '/sessions/new'
+      redirect_to new_session_path
     end
   end
 
