5aeb761eb3a251f9239369e9542dd5dec2e12fd6
vagrant <vivien.fan.1224@gmail.com>
2017-06-09 17:23:31 +0000

    controllers

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+  before_filter: current_user
+
+  def create
+    @product = Product.find product_id
+    @review = @product.reviews.new(rating: params[:rating], description: params[:review][:description])
+    @review.user = current_user
+    if @review.save!
+      redirect_to product_path product_id
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def product_id
+    params[:product_id]
+  end
+end
