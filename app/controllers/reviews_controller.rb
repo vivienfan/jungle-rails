b51e6783321130a2bedb0e886f1499567fb2dd71
vagrant <vivien.fan.1224@gmail.com>
2017-06-09 18:55:12 +0000

    before_filter -> before action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
-  before_filter: current_user
+  before_action :current_user
 
   def create
     @product = Product.find product_id
     @review = @product.reviews.new(rating: params[:rating], description: params[:review][:description])
     @review.user = current_user
-    if @review.save!
+    if @review.save
       redirect_to product_path product_id
     else
       redirect_to root_path
