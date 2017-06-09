e71c85686ce44d2cd78fd9ba72761505bb6ba1c0
vagrant <vivien.fan.1224@gmail.com>
2017-06-09 01:11:12 +0000

    creating new reviews table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :reviews
+
   validates :first_name,      presence: true
   validates :last_name,       presence: true
   validates :email,           presence: true, uniqueness: true
