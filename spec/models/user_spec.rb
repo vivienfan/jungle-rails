38840dac76ad87355d065b2b97d891b8f21783f3
vagrant <vivien.fan.1224@gmail.com>
2017-06-12 16:00:53 +0000

    tests schema

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,21 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     before :each do
     end
+
+    xit 'should have a password and passowrd_confirmation fields' do
+    end
+
+    xit 'should have email' do
+    end
+
+    xit 'should have first name' do
+    end
+
+    xit 'should have last name' do
+    end
+
+    xit 'should have unique email (case insensitive)' do
+    end
   end
 end
+
