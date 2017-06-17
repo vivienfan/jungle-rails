ae6a3e9057282b2d2207081000932d38fd1ad1a7
vagrant <vivien.fan.1224@gmail.com>
2017-06-17 16:20:11 +0000

    action mailler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,18 +15,14 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
-    port:                 587,
-    domain:               'localhost:3000',
     user_name:            ENV['EMAIL'],
     password:             ENV['EMAIL_PASSWORD'],
     authentication:       :plain,
-    enable_starttls_auto: true
   }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
