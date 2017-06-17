100c12e512b5dd4aa137e78da94cbbfd66500f67
vagrant <vivien.fan.1224@gmail.com>
2017-06-17 16:16:34 +0000

    img assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,20 +14,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default :charset => "utf-8"
-  # config.action_mailer.smtp_settings = {
-  #   address:              "smtp.gmail.com",
-  #   port:                 587,
-  #   domain:               'localhost:3000',
-  #   user_name:            ENV['EMAIL'],
-  #   password:             ENV['EMAIL_PASSWORD'],
-  #   authentication:       :plain,
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               'localhost:3000',
+    user_name:            ENV['EMAIL'],
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -44,7 +44,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
